19094eea6a7d397b07f2dfa3158d39f4a71c0a5d
jose-mayo-3467 <132014808+jose-mayo-3467@users.noreply.github.com>
2024-10-18 08:54:59 -0600

    Create figuraGrafica.java

---
+++ practica11Polimorfismo/figuraGrafica.java
@@ -0,0 +1,44 @@
+package graphics;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Héctor Quej Cosgaya
+ * @author José Aguilar Canepa
+ * 
+ * @created 24/02/13 23:38
+ * 
+ * Esta clase representa una (muy mala) implementación de una figura
+ * geométrica. Tu misión es aplicar con éxito los principios del Polimorfismo
+ * que aprendiste anteriormente. ¡Suerte!
+ */
+public abstract class FiguraGrafica {
+    
+    protected String figura;
+    
+    public FiguraGrafica(){
+        this("cuadrado");
+    }
+    
+    public FiguraGrafica(String figura){
+        this.figura = figura;
+    }
+
+    public abstract void dibujar(Graphics g);
+    
+    
+    public abstract void encojer(Graphics g);
+    
+    
+    public abstract void agrandar(Graphics g);
+    
+    public void cambiarColor(Graphics g, Color c){
+        g.setColor(c);
+        this.dibujar(g);
+    }
+    
+    public abstract void mover(Graphics g, String direccion);
+}
